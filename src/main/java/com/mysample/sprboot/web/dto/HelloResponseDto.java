5c2062d7f5defa9896f250eed466f3d5abbd2b2f
khnam <khnam@midasit.com>
2020-05-10 12:14:45 +0900

    샘플dto와 2장까지 스터이 완료

--- src/main/java/com/mysample/sprboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/mysample/sprboot/web/dto/HelloResponseDto.java
@@ -1,14 +1,18 @@
 package com.mysample.sprboot.web.dto;
 
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@Setter
+@RequiredArgsConstructor
 public class HelloResponseDto {
     private final String name;
     private final int amount;
 
+    /*
     public HelloResponseDto(String name, int amount) {
         this.name = name;
         this.amount = amount;
     }
+    */
 }
